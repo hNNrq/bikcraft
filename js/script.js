b41ac08a4e7d67f8b3e3b5b9b694720132d6f81c
Henrique Norman <henriquenorman@gmail.com>
2025-04-03 21:00:23 -0300

    parametros-URL

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//Ativar Links do Menu
+
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -11,3 +13,15 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+//Ativar Items do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
